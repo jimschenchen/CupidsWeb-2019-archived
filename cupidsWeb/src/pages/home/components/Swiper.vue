b36da6a9c44b9e7b914c1758e660063eb7b544fb
jimschenchen <jimschenchen@163.com>
2019-08-17 19:44:31 +0800

    update

---
+++ cupidsWeb/src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="spwiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566045701168&di=50911dc3ab7b8cef0c6d725fce6ac3b4&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Fda2dd1cca8fa0a60201d0a1c0fbde87e3844ceaf.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566045738307&di=abeca06a2f802b867e74090b53271a31&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F8adb84f584c9067123f257eca76915c7abd98c74.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper >>> .swiper-pagination
+    bottom: 1.4rem
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 55%
+    background: #eee
+    .spwiper-img
+      width: 100%
+</style>
