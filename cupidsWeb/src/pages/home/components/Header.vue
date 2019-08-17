e09c4e7935cdd3ce9a161dceb67ec5b8aedcb275
jimschenchen <jimschenchen@163.com>
2019-08-17 17:37:46 +0800

    update

---
+++ cupidsWeb/src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-align">&#xe658;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe63c;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">
+       城市
+      <span class="iconfont arrow-icon">&#xe65c;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: 0.64rem
+      float: left
+      .back-align
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      padding-left: .2rem
+      margin-top: .12rem
+      margin-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.1rem
+        font-size: .30rem
+
+</style>
