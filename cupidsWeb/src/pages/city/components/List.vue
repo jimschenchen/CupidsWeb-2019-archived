653d0e0c35d6b95ffa32dd21c580242057399ca6
jimschenchen <jimschenchen@163.com>
2019-08-18 21:28:05 +0800

    dynamic data render

--- cupidsWeb/src/pages/city/components/List.vue
+++ cupidsWeb/src/pages/city/components/List.vue
@@ -12,42 +12,24 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
+
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title border-topbottom">
+          {{key}}
+        </div>
         <div class="item-list">
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
+          <div class="item border-bottom" v-for="innerItem in item" :key="innerItem.id">
+            {{innerItem.name}}
           </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">B</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">C</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-          <div class="item border-bottom">阿鲁巴</div>
-        </div>
-      </div>
+
     </div>
   </div>
 </template>
@@ -57,6 +39,10 @@ import BScroll from 'better-scroll'
 
 export default {
   name: 'CityL',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
   }
