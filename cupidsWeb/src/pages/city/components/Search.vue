4eeeecb6167ba1d681d08baae7b6af9cde9c16e8
jimschenchen <jimschenchen@163.com>
2019-08-18 19:48:13 +0800

    search

---
+++ cupidsWeb/src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或者其他" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    background: $bgColor
+    padding: 0 .2rem
+    .search-input
+      box-sizing: border-box
+      padding: 0 .1rem
+      width: 100%
+      height: .62rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
